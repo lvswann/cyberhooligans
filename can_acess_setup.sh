d365a75c49383887f1b50a17a1c461d97fb60da1
lvswann <90424627+lvswann@users.noreply.github.com>
2023-12-12 17:50:30 -0500

    Update can_acess_setup.sh

--- can_acess_setup.sh
+++ can_acess_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# run on Linux command line after connecting to J1939 port to set up connection to can bus
+# run can dump can0 to test 
+# you should see a stream of data
+
+# may need to change bit rate based on car 
 sudo ip link set can0 type can bitrate 125000
 sudo ip link set up can0
